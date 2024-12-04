7cc1472dcabc13f27624a9dc2b7308413520a052
Mariana <mariianaf3@gmail.com>
2024-12-04 17:31:39 -0300

    Se implementaron todas las funcionalidades necesarias

--- sitio_web/views.py
+++ sitio_web/views.py
@@ -18,7 +18,7 @@ def index(request):
         qr = qrcode.QRCode(
             version=1,
             error_correction=qrcode.constants.ERROR_CORRECT_L,
-            box_size=10,
+            box_size=5,
             border=4,
         )
         qr.add_data(apk_url)
